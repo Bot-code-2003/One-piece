b59578fe638e3f6508e431444ea1180d0a2fee44
Neha R <neha@Nehas-MacBook-Air.local>
2024-05-16 13:42:02 +0530

    Final_final

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:4000/profile", {
+        const response = await axios.get("https://one-piece-api-snowy.vercel.app/profile", {
           withCredentials: true,
         });
         /* response.data:
@@ -38,7 +38,7 @@ const Navbar = () => {
   async function logout() {
     try {
       // Clears the token cookie in Server-side
-      await axios.post("http://localhost:4000/logout", {
+      await axios.post("https://one-piece-api-snowy.vercel.app/logout", {
         withCredentials: true,
       });
       setUserInfo(null);
